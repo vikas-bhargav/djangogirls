894122d58eb0a60d50a2b37a7ae3d65dded191e0
vikas.bhargav <vikasbhargav0@gmail.com>
2017-12-15 12:59:07 +0530

    Modified template for display posts from databse.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    context = {}
+    posts = Post.objects.all().order_by('-published_date') # (published_date=timezone.now()) # .order_by('published_date')
+    if posts is not None:
+        context['posts'] = posts
+    return render(request, 'blog/post_list.html', context)
