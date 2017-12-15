23e0b05ec316252650e14f7ee61c7cb4130f7b4c
vikas.bhargav <vikasbhargav0@gmail.com>
2017-12-15 12:18:21 +0530

    Change to HTML for site.

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c4t1-8j1*rpc5e=abc35tsy&6v#&d5css3u3t2wezfx(15$sem'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','vikas19073.pythonanywhere.com']
 
 
 # Application definition
