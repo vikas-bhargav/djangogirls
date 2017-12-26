814f8929971875564b2a8caf935a76ab80a750b5
vikas.bhargav <vikasbhargav0@gmail.com>
2017-12-26 12:44:41 +0530

    Added login, logout, edit, delete,detail posts functionality.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 # Create your views here.
 
-
+@login_required
 def post_list(request):
     context = {}
     posts = Post.objects.all().order_by('-published_date')
@@ -12,14 +13,14 @@ def post_list(request):
         context['posts'] = posts
     return render(request, 'blog/post_list.html', context)
 
-
+@login_required
 def post_detail(request, pk):
     context = {}
     post = get_object_or_404(Post, pk=pk)
     context['post'] = post
     return render(request, 'blog/post_detail.html', context)
 
-
+@login_required
 def post_new(request):
     form = PostForm()
     context = {}
@@ -28,7 +29,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -36,7 +37,7 @@ def post_new(request):
         context['form'] = form
     return render(request, 'blog/post_new.html', context)
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -44,9 +45,26 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_new.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
